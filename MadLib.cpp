cca4a3cfe9ef79bb503032e9c070f5ffb9c4f172
Domzori <noahhoffer@gmail.com>
2023-01-28 11:00:24 -0700

    project requirements met

--- MadLib.cpp
+++ MadLib.cpp
@@ -10,41 +10,116 @@ int main()
                part_Of_The_Body_Plural, verb4, verb5;
 
 
-        cout<<"Pick an adjective.";
+        cout<<"Pick an adjective ";
         cin>> adj1;
-        cout<<"Pick a noun.";
+        cout<<"Pick a noun ";
         cin>>noun1;
-        cout<<"Choose a different noun.";
+        cout<<"Choose a different noun ";
         cin>>noun2;
-        cout<<"Now pick a verb.";
+        cout<<"Now pick a verb ";
         cin>>verb1;
-        cout<<"Give me a type of liquid.";
+        cout<<"Give me a type of liquid ";
         cin>>type_Of_Liquid1;
-        cout<<"And another one.";
+        cout<<"And another one ";
         cin>>type_Of_Liquid2;
-        cout<<"Just one more type of liquid.";
+        cout<<"Just one more type of liquid ";
         cin>>type_Of_Liquid3;
-        cout<<"Alright done with those now give me a color.";
+        cout<<"Alright done with those now give me a color ";
         cin>>color1;
-        cout<<"Ok that's good but now I need another verb";
+        cout<<"Ok that's good but now I need another verb ";
         cin>>verb2;
-        cout<<"Need another verb here";
+        cout<<"Need another verb here ";
         cin>>verb3;
-        cout<<"That's enough verbs for now, we'll come back for more later. Give me a number.";
+        cout<<"That's enough verbs for now, we'll come back for more later. Give me a number ";
         cin>>number1;
-        cout<<"Alright can I get an adjective?";
+        cout<<"Alright can I get an adjective ";
         cin>>adj2;
-        cout<<"One adverb please.";
+        cout<<"One adverb please ";
         cin>>adverb1;
-        cout<<"This is a strange one but can I have a plural part of the body";
+        cout<<"This is a strange one but can I have a plural part of the body ";
         cin>>part_Of_The_Body_Plural;
-        cout<<"Back in verb country lets wrap this up, give me a verb!";
+        cout<<"Back in verb country lets wrap this up, give me a verb ";
         cin>>verb4;
-        cout<<"Last one, one more verb for the road.";
+        cout<<"Last one, one more verb for the road ";
         cin>>verb5;
+        cout<<endl<<endl<<endl<<endl;
 
-        cout<<"Here's what we got.\n";
-        cout<<"To make "<<adj1<<" ice cubes, first find a tray with a mold shaped like a/n "<<noun1<<" or an "<<noun2<<".\n";
-        cout<<" Then "<<verb1<<" once while holding the tray. Fill the molds with "<<type_Of_Liquid1<<" or even "<<type_Of_Liquid2<<"\n"
-        cout<<"(but for best results, always use "<<type_Of_Liquid3<<". It can even be dyed "<<color1<<" if you wish.\n"
+        cout<<"Here's what we got."<<endl;
+        cout<<"To make "<<adj1<<" ice cubes, first find a tray with a mold shaped like a/an "<<noun1<<" or a/an "<<noun2<<".\n";
+        cout<<"Then "<<verb1<<" once while holding the tray. Fill the molds with "<<type_Of_Liquid1<<" or even "<<type_Of_Liquid2<<"\n";
+        cout<<"(but for best results, always use "<<type_Of_Liquid3<<". It can even be dyed "<<color1<<" if you wish.\n";
+        cout<<verb2<<" carefully when placing the tray in the freezer. Allow cubes to "<<verb3<<" for at least "<<number1<<",\n";
+        cout<<"minutes, or until they are completely "<<adj2<<". Remove the tray "<<adverb1<<endl;
+        cout<<"and jiggle with your "<<part_Of_The_Body_Plural;
+        cout<<" until the cubes "<<verb4<<". Add to your favorite drink and ";
+        cout<<verb5<<".";
+
     }
+
+
+
+
+    /* %%%%%%%%%%SAMPLE RUN #1%%%%%%%%%%
+    Pick an adjective old
+    Pick a noun dog
+    Choose a different noun cat
+    Now pick a verb run
+    Give me a type of liquid water
+    And another one milk
+    Just one more type of liquid soda
+    Alright done with those now give me a color red
+    Ok that's good but now I need another verb hop
+    Need another verb here skip
+    That's enough verbs for now, we'll come back for more later. Give me a number 23
+    Alright can I get an adjective young
+    One adverb please soon
+    This is a strange one but can I have a plural part of the body ears
+    Back in verb country lets wrap this up, give me a verb jump
+    Last one, one more verb for the road smack
+
+
+
+
+    Here's what we got.
+    To make old ice cubes, first find a tray with a mold shaped like a/an dog or a/an cat.
+    Then run once while holding the tray. Fill the molds with water or even milk
+    (but for best results, always use soda. It can even be dyed red if you wish.
+    hop carefully when placing the tray in the freezer. Allow cubes to skip for at least 23,
+    minutes, or until they are completely young. Remove the tray soon
+
+
+
+
+    %%%%%%%%%%SAMPLE RUN #2%%%%%%%%%%
+    Pick an adjective smelly
+    Pick a noun tree
+    Choose a different noun house
+    Now pick a verb fly
+    Give me a type of liquid brine
+    And another one vodka
+    Just one more type of liquid gas
+    Alright done with those now give me a color orange
+    Ok that's good but now I need another verb spin
+    Need another verb here flee
+    That's enough verbs for now, we'll come back for more later. Give me a number 99
+    Alright can I get an adjective pristine
+    One adverb please happily
+    This is a strange one but can I have a plural part of the body fists
+    Back in verb country lets wrap this up, give me a verb flail
+    Last one, one more verb for the road roll
+
+
+
+
+    Here's what we got.
+    To make smelly ice cubes, first find a tray with a mold shaped like a/an tree or a/an house.
+    Then fly once while holding the tray. Fill the molds with brine or even vodka
+    (but for best results, always use gas. It can even be dyed orange if you wish.
+    spin carefully when placing the tray in the freezer. Allow cubes to flee for at least 99,
+    minutes, or until they are completely pristine. Remove the tray happily
+    and jiggle with your fists until the cubes flail. Add to your favorite drink and roll.
+
+    */
+
+
+
